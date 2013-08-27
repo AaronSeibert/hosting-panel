f788a726178855271c11daf85177d4a5f019eadf
Aaron Seibert <aaron@binaryitsystems.com>
2013-08-27 19:37:28 -0400

    Started building logic for obtaining and syncing plans between local and remote.

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,43 @@
 class Plan < ActiveRecord::Base
+  
+  
+  def self.get_remote_plans
+    @plans = Stripe::Plan.all
+    
+    i = 0
+    num = @plans.count()
+    p = Hash.new
+    while i < num do
+      
+      h = { @plans.data[i].id => {:price => @plans.data[i].amount, :description => @plans.data[i].name}}
+      
+      p.merge!(h)
+
+      i = i + 1
+    end
+    return p
+  end
+  
+  def self.sync_remote
+    @remote = self.get_remote_plans
+    @local = Plan.all
+
+    @remote.keys.each do |k|
+      if @local.exists?(:remote_id => k)
+        puts "Remote plan already exists locally."
+      else
+        r = @remote[k]
+        puts "Creating local plan #{k}."
+        l = Plan.new
+        l[:remote_id] = k
+        puts "Adding remote price to plan: #{r[:price]}"
+        l[:price] = r[:price]
+        puts "Adding remote description to plan: #{r[:description]}"
+        l[:description] = r[:name]
+        if l.save
+          puts "Plan saved successfully."
+        end
+      end
+    end
+  end
 end
