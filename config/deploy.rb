badc580330ee6ea14fb742bacad7216d92313ccb
Aaron Seibert <aaron@binaryitsystems.com>
2014-03-08 18:03:46 -0500

    Cap 3 for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,58 @@
-require 'bundler/capistrano'
-set :user, 'aaron.seibert'
-set :applicationdir, "/var/rails_apps/#{domain}"
-set :gateway, 'aaron.seibert@ewrpmgmt01.binaryitsystems.com'
+# config valid only for Capistrano 3.1
+lock '3.1.0'
 
-set :scm, 'git'
-set :repository,  "https://github.com/Lateral-October/hosting-panel.git"
-set :git_enable_submodules, 1 # if you have vendored rails
-set :branch, 'master'
-set :git_shallow_clone, 1
-set :scm_verbose, true
-  
-# roles (servers)
-role :web, "ewrpweb01a.binaryitsystems.com", "ewrpweb01b.binaryitsystems.com", :no_release => true
-role :app, "ewrpweb01a.binaryitsystems.com", "ewrpweb01b.binaryitsystems.com"
-role :db,  "ewrpweb01a.binaryitsystems.com", :primary => true
-   
-# deploy config
-set :deploy_to, applicationdir
-set :deploy_via, :export
-    
-# additional settings
-default_run_options[:pty] = true  # Forgo errors when deploying from windows
-#ssh_options[:keys] = %w(/home/user/.ssh/id_rsa)            # If you are using ssh_keysset :chmod755, "app config db lib public vendor script script/* public/disp*"set :use_sudo, false
-     
-# Passenger
+set :application, 'bitshosting'
+set :repo_url, 'git@github.com:Lateral-October/hosting-panel.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
   end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
 end
