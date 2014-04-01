94a032279508ae2d342c9fc03dd199561247dd96
Aaron Seibert <aaron@binaryitsystems.com>
2014-04-01 01:35:07 -0400

    corrected env test

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'rufus/scheduler'
 
 
-if `hostname -f` == 'ewrpweb01a.binaryitsystems.com' || Rails.env.development
+if `hostname -f` == 'ewrpweb01a.binaryitsystems.com' || Rails.env == "production"
   scheduler = Rufus::Scheduler.new
   
   scheduler.cron '0 0 * * *' do
